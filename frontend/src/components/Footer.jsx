e10edc9c069879b6572c7c60dd4a9832e66815fb
ptfernandez0724 <fernandezpaulo0724@gmail.com>
2024-10-25 18:43:30 -0400

    updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
             <footer class=" shadow m-4">
                 <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <a href="https://flowbite.com/" class=" mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="../../public/favicon.ico" class="h-8" alt="PF Logo" />
+                    <a href="https://ptfernandez0724.github.io/csp1/" class=" mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <img src="../../public/favicon.png" class="h-8" alt="PF Logo" />
                     </a>
                     <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/ptfernandez0724" class="hover:underline">Created by Paulo Fernandez</a>. All Rights Reserved.</span>
                 </div>
