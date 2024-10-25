e803b85952e2ea36e97dfbe0f816f345be81dec1
ptfernandez0724 <fernandezpaulo0724@gmail.com>
2024-10-25 18:38:37 -0400

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js"
